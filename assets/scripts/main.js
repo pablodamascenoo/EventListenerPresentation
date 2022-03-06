921dd56b1b37f525f665bb06056b3eb4ba7eea53
pablodamascenoo <pablofacex@gmail.com>
2022-03-06 13:53:12 -0300

    feat: added bubbling and capture events

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,9 +6,9 @@ primaryButton.addEventListener("click", ()=>{
 
     for(let i=0; i<articles.length-1; i++){
         if(!articles[i].classList.contains("disabled")){
-            if(i===0) button.innerText = "Proximo"
             window.classList.add("animation-window")
             setTimeout(()=>{
+                if(i===0) primaryButton.innerText = "Proximo"
                 articles[i].classList.toggle("disabled")
                 articles[i+1].classList.toggle("disabled")
                 window.classList.add("animation-down-class")
@@ -24,13 +24,23 @@ primaryButton.addEventListener("click", ()=>{
 
 let captureDiv = document.querySelector(".capture")
 let innerCaptureDiv = captureDiv.querySelector("div")
+let bubblingDiv = document.querySelector(".bubbling")
+let innerBubblingDiv = bubblingDiv.querySelector("div")
 
 captureDiv.addEventListener("click", async function(){
-    alert("Eu venho do elemento pai")
-    innerCaptureDiv.querySelector("p").innerText = "Eu venho primeiro"
+    innerCaptureDiv.querySelector("p").innerHTML = "1º Elemento pai<br>"
+    innerCaptureDiv.querySelector("p").classList.add("fade")
 }, true)
 
 innerCaptureDiv.addEventListener("click", ()=>{
-    alert("Eu venho do elemento filho")
-    innerCaptureDiv.querySelector("p").innerText = "Eu venho depois"
+    innerCaptureDiv.querySelector("p").innerHTML += "2º Elemento filho"
 }, true)
+
+bubblingDiv.addEventListener("click", async function(){
+    innerBubblingDiv.querySelector("p").innerHTML += "2º Elemento pai"
+}, false)
+
+innerBubblingDiv.addEventListener("click", ()=>{
+    innerBubblingDiv.querySelector("p").innerHTML = "1º Elemento filho<br>"
+    innerBubblingDiv.querySelector("p").classList.add("fade")
+}, false)
